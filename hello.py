6e0727832b330f688337b75dd7ac3be53b1071fa
lisj <135824mm>
2019-10-28 14:33:46 +0800

    测试提交

---
+++ hello.py
@@ -0,0 +1,14 @@
+# i=10
+# sum=0
+# while(i<20):
+#     sum = sum + i
+#     i = i + 1
+# print(sum)
+
+a = dict()
+a['name'] = 'li'
+a['age'] = 23
+
+print(a['name'])
+if a.get('tel') is not None:
+    print(a['tel'])
